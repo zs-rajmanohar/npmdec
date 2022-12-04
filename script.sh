34491d7a0562c5c2bf2be6ef4cfd675a8f61f12d
zs-rajmanohar <82603629+zs-rajmanohar@users.noreply.github.com>
2022-12-04 13:34:51 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,7 +1,4 @@
-for k in $(seq 1 60)
-do
-    ipaddress=$(curl -s http://ipinfo.io/ip)
-    echo $ipaddress
-    npm i express --save
-    node index.js
-done
+ipaddress=$(curl -s http://ipinfo.io/ip)
+echo $ipaddress
+npm i express --save
+node index.js
